d750eb42673c926eb27a88f00950ca08176943fd
Rajneesh Shukla <124131361+developerrajneesh@users.noreply.github.com>
2024-02-03 11:08:17 +0530

    Update Source.js

--- src/app/Source.js
+++ src/app/Source.js
@@ -1,2 +1,3 @@
-export  const serverUrl = 'https://cpapi.rajneesh.site'
+// export  const serverUrl = 'https://cpapi.rajneesh.site'
+export  const serverUrl = 'https://cpanel-backend-rajneesh.onrender.com'
 // export  const serverUrl = 'http://localhost:5000'
